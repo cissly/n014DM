606a38624f0a652ef0fc1954cab7506f01b1c33c
cissly <tkdwls237@naver.com>
2024-02-14 18:05:53 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 const express = require('express')
 const path = require('path')
-
 const app = express();
+const http = require('http');
+const {Server} = require('socket.io');
+const { socket } = require('server/router');
+const { default: mongoose } = require('mongoose');
+const server = http.createServer(app);
+const io = new Server(server)
+
+mongoose.set('strictQuery', false)
+mongoose.connect('mongodb+srv://tkdwls237:yDB9IRFFwFLY2hQp@cluster0.rcpcfst.mongodb.net/?retryWrites=true&w=majority').then(()=> {
+    console.log('DB연결 성공')
+}).catch(err => { console.error(err)})
+
+let users = [];
+io.on('connection', async socket => {
+    let userData = {};
+    users.push(userData);
+    io.emit('users-data', {users})
+
+    socket.on('message-to-server',()=> {
+
+    })
+
+    socket.on('fetch-messages', ()=> {
+
+    })
+    socket.on('disconnect', () => {
+
+    })
+})
 
 const port = 4000;
 
@@ -11,6 +39,6 @@ app.use(express.static(publicDirectory));
 app.use(express.json())
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server is up on port' + port)  
 })
