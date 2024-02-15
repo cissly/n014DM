ce40826df9e49861fdb0d675c524e3d0221a8e7b
cissly <tkdwls237@naver.com>
2024-02-15 17:40:03 +0900

    Session&Chat

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
-const socket= io('https://localhost:4000/', {
+const socket= io('http://localhost:4000/', {
     autoConnect: false
-});
+})
 
 socket.onAny((event, ...args) => {
     console.log(event, ...args)
@@ -15,3 +15,161 @@ const userTagline = document.querySelector('#users-tagline');
 const title = document.querySelector('#active-user');
 const messages = document.querySelector('.messages');
 const msgDiv = document.querySelector('.msg-form');
+
+const loginForm = document.querySelector('.user-login');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const username = document.getElementById('username');
+    createSession(username.value.toLowerCase());
+    username.value = '';
+})
+
+
+const createSession = async (username) => {
+    const options = {
+        method: 'Post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username })
+    }
+    await fetch('/session', options)
+    .then(res => res.json())
+    .then(data => {
+
+        socketConnect(data.username, data.userID);
+
+        // localStorage에 세션을 Set
+        localStorage.setItem('session-username', data.username);
+        localStorage.setItem('session-userID', data.userID);
+
+        loginContainer.classList.add('d-none');
+        chatBody.classList.remove('d-none');
+        userTitle.innerHTML = data.username;
+    })
+    .catch(err => console.error(err));
+}
+
+const socketConnect = async (username, userID) => {
+    socket.auth = { username, userID };
+
+    await socket.connect();
+}
+
+
+const setActiveUser = (element, username, userID) => {
+    title.innerHTML = username;
+    title.setAttribute('userID', userID);
+
+    const lists = document.getElementsByClassName('socket-users');
+    for(let i = 0; i < lists.length ; i++){
+        lists[i].classList.remove('table-active')
+    }
+
+    element.classList.add('table-active');
+
+    msgDiv.classList.remove('d-none');
+    messages.classList.remove('d-none');
+    messages.innerHTML = '';
+    socket.emit('fetch-messages', { receiver: userID});
+    const notify = document.getElementById(userID);
+    notify.classList.add('d-none')
+}
+
+const appendMessage = ({ message, time, background, position}) => {
+    let div = document.createElement('div');
+    div.classList.add('message','bg-opacity-25', 'm-2', 'px-2', 'py-1',background, position)
+    div.innerHTML = `<span class="msg-text">${message} </span> <span class="msg-time">${time}</span>`
+    messages.append(div);
+    messages.scrollTo(0, messages.scrollHeight);
+}
+
+socket.on('users-data', ({ users }) => {
+    const index = users.findIndex(user => user.userID === socket.id)
+    if(index > -1) {
+        users.splice(index, 1);
+    }
+    // user table list 생성하기
+    userTable.innerHTML = '';
+    let ul = `<table class="table table-hover">`;
+    for (const user of users) {
+        ul += `<tr class="socket-users" onclick="setActiveUser(this, '${user.username}', '${user.userID}')"><td>${user.username}<span class="text-danger ps-1 d-none" id="${user.userID}">!</span></td></tr>`
+    }
+    ul += `</table>`;
+    if (users.length > 0) {
+        userTable.innerHTML = ul;
+        userTagline.innerHTML = '접속 중인 유저';
+        userTagline.classList.remove('text-danger');
+        userTagline.classList.add('text-success');
+    } else {
+        userTagline.innerHTML = '접속 중인 유저 없음';
+        userTagline.classList.remove('text-success');
+        userTagline.classList.add('text-danger');
+    }
+})
+
+const sessUsername = localStorage.getItem('session-username')
+const sessUserID = localStorage.getItem('session-userID')
+
+if(sessUserID && sessUsername){
+    socketConnect(sessUsername,sessUserID);
+
+    loginContainer.classList.add('d-none');
+    chatBody.classList.remove('d-none');
+    userTitle.innerHTML = sessUsername
+}
+
+const mesForm = document.querySelector('.msgFrom')
+const message = document.getElementById('message')
+
+
+
+mesForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const to = title.getAttribute('userID');
+    const time = new Date().toLocaleString('en-US', {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true
+    })
+
+    const payload = {
+        from: socket.id,
+        to,
+        message: message.value,
+        time
+    }
+
+    socket.emit('message-to-server', payload);
+
+    appendMessage({...payload, background: 'bg-success', position: 'right'});
+
+    message.value = '';
+    message.focus();
+})
+localStorage
+
+
+socket.on('message-to-client', ({from,message,time}) => {
+    const receiver = title.getAttribute('userID')
+    const notify = document.getElementById(from)
+    if(receiver === null) {
+        notify.classList.remove('d-none')
+    } else if ( receiver === from){
+        appendMessage({
+            message,
+            time,
+            background: 'bg-secondary',
+            position: 'left'
+        })
+    } else {
+        notify.classList.remove('d-none')
+    }
+}) 
+
+socket.on('user-away', userID => {
+    const to = title.getAttribute('userID');
+    if(to === userID){
+        title.innerHTML = '&nbsp;'
+        msgDiv.classList.add('d-none')
+        messages.classList.add('d-none')
+    }
+})
--- src/index.js
+++ src/index.js
@@ -1,44 +1,73 @@
 const express = require('express')
 const path = require('path')
 const app = express();
+const crypto = require('crypto');
+
 const http = require('http');
 const {Server} = require('socket.io');
-const { socket } = require('server/router');
 const { default: mongoose } = require('mongoose');
+const {saveMessages} = require('./utils/messages')
 const server = http.createServer(app);
 const io = new Server(server)
 
-mongoose.set('strictQuery', false)
+const publicDirectory = path.join(__dirname, "../public")
+app.use(express.static(publicDirectory));
+app.use(express.json())
+
 mongoose.connect('mongodb+srv://tkdwls237:yDB9IRFFwFLY2hQp@cluster0.rcpcfst.mongodb.net/?retryWrites=true&w=majority').then(()=> {
     console.log('DB연결 성공')
 }).catch(err => { console.error(err)})
 
+
+const randomId = () => crypto.randomBytes(8).toString('hex');
+
+
+app.post('/session', (req, res) => {
+    const data = {
+        username: req.body.username,
+        userID: randomId()
+    }
+    res.send(data);
+})
+io.use((socket, next) => {
+    const username = socket.handshake.auth.username
+    const userID = socket.handshake.auth.userID
+    if(!username){
+        return next(new Error('Invalid username'))
+    }
+    socket.username = username;
+    socket.id = userID
+
+    next();
+})
+
+
 let users = [];
 io.on('connection', async socket => {
-    let userData = {};
+    let userData = {
+        username: socket.username,
+        userID: socket.id
+    };
     users.push(userData);
     io.emit('users-data', {users})
 
-    socket.on('message-to-server',()=> {
-
+    socket.on('message-to-server',(payload)=> {
+        io.to(payload.to).emit('message-to-client', payload);
+        saveMessages(payload);
     })
 
     socket.on('fetch-messages', ()=> {
 
     })
     socket.on('disconnect', () => {
+        users = users.filter(user => user.userID !== socket.id)
 
+        io.emit('users-data', {users})
+        io.emit('user-away', socket.id)
     })
 })
 
 const port = 4000;
-
-const publicDirectory = path.join(__dirname, "../public")
-
-app.use(express.static(publicDirectory));
-app.use(express.json())
-
-
 server.listen(port, () => {
     console.log('Server is up on port' + port)  
 })
